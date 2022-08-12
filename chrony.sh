21e725791a8c94cd302b29c526ed0eb430ef5904
Timothy Yu <tinyau.yu@outlook.com>
2022-08-12 16:53:02 -0700

    correcting QT development installation

--- chrony.sh
+++ chrony.sh
@@ -5,6 +5,8 @@ set -eu -o pipefail # fail on error and report it, debug all lines
 sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
+echo ####################################
+echo
 echo installing chrony
 
 apt-get -y install chrony
@@ -20,3 +22,4 @@ systemctl enable chrony
 systemctl restart chrony
 chronyc sources
 chronyc tracking
+echo
--- packages_install.sh
+++ packages_install.sh
@@ -5,7 +5,9 @@ set -eu -o pipefail # fail on error and report it, debug all lines
 sudo -n true
 test $? -eq 0 || exit 1 "you should have sudo privilege to run this script"
 
+echo
 echo installing missing packages
+echo
 
 apt -y install ssh tcsh ethtool ncat rsync strace net-tools sysstat smartmontools lm-sensors traceroute time minicom screen git lsof
 apt -y install lsb-release
@@ -13,7 +15,6 @@ apt -y install flex bison
 apt -y install neofetch
 apt -y install snmp snmp-mibs-downloader
 apt -y install git subversion g++ gfortran cmake doxygen
-apt -y install python
 apt -y install curl libcurl4 libcurl4-openssl-dev
 apt -y install mariadb-client ### mysql client
 apt -y install libz-dev sqlite3 libsqlite3-dev unixodbc-dev
@@ -31,7 +32,7 @@ apt -y install libusb-dev libusb-1.0-0-dev
 apt -y install xfig gsfonts-x11 gsfonts-other # install fonts for xfig
 apt -y install libjson-perl
 apt -y install libgsl-dev # additional GNU Scientific Library
-apt -y install qt5-default # Qt development
+apt -y install qtcreator qtbase5-dev qt5-qmake # Qt development
 apt -y install python3.8-full python3.8-dev python3.8-dbg python3-pip ### for pyROOT
 apt -y install imagemagick imagemagick-common ckeditor # for elog
 apt -y install linux-tools-common linux-tools-generic # cpupower frequency-info
